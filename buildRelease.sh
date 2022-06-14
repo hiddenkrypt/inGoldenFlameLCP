9ea9c17f7cd7922bb99cedbb051a0b5870451aab
Ada Clark <hiddenkrypt@gmail.com>
2022-06-14 00:41:13 -0700

    Added last base exotic weapons, pilot gear, and mech systems

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ver=$(cat lcp_manifest.json | pcregrep -o1  '.*version" ?: ?"([0-9\.]*)');
-zip releases/igf-build.$ver.lcp backgrounds.json weapons.json lcp_manifest.json npc_classes.json npc_features.json
+zip releases/igf-build.$ver.lcp backgrounds.json weapons.json systems.json lcp_manifest.json npc_classes.json npc_features.json pilot_gear.json
 
 
