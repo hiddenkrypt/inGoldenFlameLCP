4a0e10e9a58edd3142b7a154461082aaae754bc7
Ada Clark <hiddenkrypt@gmail.com>
2022-06-20 23:31:59 -0700

    Added cardcound system from mission 2, and redid build system to allow multiple lcp collections out

--- buildRelease.sh
+++ buildRelease.sh
@@ -16,7 +16,24 @@ close_files () {
   echo "]" >> systems.json
   echo "]" >> weapons.json
 }
-
+delete_files () {
+  touch actions.json
+  touch backgrounds.json
+  touch lcp_manifest.json
+  touch npc_classes.json
+  touch npc_features.json
+  touch pilot_gear.json
+  touch systems.json
+  touch weapons.json
+  rm actions.json
+  rm backgrounds.json
+  rm lcp_manifest.json
+  rm npc_classes.json
+  rm npc_features.json
+  rm pilot_gear.json
+  rm systems.json
+  rm weapons.json
+}
 
 #ver=$(cat lcp_manifest.json | pcregrep -o1  '.*version" ?: ?"([0-9\.]*)');
 #zip releases/igf-build.$ver.lcp 
@@ -39,16 +56,38 @@ echo "creating igf-a1m1-build.$ver.lcp"
 zip releases/igf-a1m1-build.$ver.lcp actions.json backgrounds.json lcp_manifest.json npc_classes.json npc_features.json pilot_gear.json systems.json weapons.json
 
 
+echo "Building gear files"
+echo "==>   Building Cardcount"
+reset_files
+cp a1m2_cardcount_lcp_manifest.json lcp_manifest.json
+cat a1m2_cardcount_systems.json >> systems.json
+close_files
+ver=$(cat lcp_manifest.json | pcregrep -o1  '.*version" ?: ?"([0-9\.]*)');
+touch releases/igf-a1m2-cardcount.$ver.lcp
+rm releases/igf-a1m2-cardcount.$ver.lcp
+echo "creating igf-a1m2-cardcount.$ver.lcp"
+zip releases/igf-a1m2-cardcount.$ver.lcp  lcp_manifest.json systems.json
 
+echo "Building Total File"
+reset_files
 
+cat a1m1_actions.json >> actions.json
+cp a1m1_backgrounds.json backgrounds.json
+cp total_lcp_manifest.json lcp_manifest.json
+cat a1m1_npc_classes.json >> npc_classes.json
+cat a1m1_npc_features.json >> npc_features.json
+cat a1m1_pilot_gear.json >> pilot_gear.json
+cat a1m1_systems.json >> systems.json
+echo "," >> systems.json
+cat a1m2_cardcount_systems.json >> systems.json
+cat a1m1_weapons.json >> weapons.json
+close_files
+ver=$(cat lcp_manifest.json | pcregrep -o1  '.*version" ?: ?"([0-9\.]*)');
+touch releases/igf-total.$ver.lcp
+rm releases/igf-total.$ver.lcp
+echo "creating igf-total.$ver.lcp"
+zip releases/igf-total.$ver.lcp actions.json backgrounds.json lcp_manifest.json npc_classes.json npc_features.json pilot_gear.json systems.json weapons.json
 
 echo "Final Cleanup";
-rm actions.json
-rm backgrounds.json
-rm lcp_manifest.json
-rm npc_classes.json
-rm npc_features.json
-rm pilot_gear.json
-rm systems.json
-rm weapons.json
+delete_files
 
